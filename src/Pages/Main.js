1d2fe8dbc36d1e3dec45dd6a83ac9394e5923e8b
Arthur Queiroz Claizoni <arthurclaizoni@gmail.com>
2019-09-09 19:23:46 -0300

    maps region to user location

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import {
-  TouchableOpacity,
   PermissionsAndroid,
   ToastAndroid,
   View,
@@ -8,77 +7,68 @@ import {
   StyleSheet,
 } from 'react-native';
 import MapView from 'react-native-maps';
+import Geocoder from 'react-native-geocoding';
+import Geolocation from 'react-native-geolocation-service';
+
+Geocoder.init('AIzaSyAws3DiTDOsKOtriFEzepkD5pBysglvgkA');
 
 export default function Main() {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [contador, setContador] = useState(0);
-  const [region, setRegion] = useState({
-    region: null,
-  });
+  const [requestMapCameraChange, SetRequestMapCameraChange] = useState(false);
+  const [locationGaranted, setLocationGaranted] = useState(false);
+  const [region, setRegion] = useState(null);
+  const [userLocation, setUserLocation] = useState(null);
+  const [userAddress, setUserAddress] = useState(null);
 
   useEffect(() => {
-    if (isLoaded === true) {
-      alert('mapa carregado');
-      teste();
-    }
-  }, [isLoaded]);
+    if (locationGaranted && requestMapCameraChange) {
+      Geolocation.getCurrentPosition(
+        async ({coords: {latitude, longitude}}) => {
+          const response = await Geocoder.from({latitude, longitude});
+          const address = response.results[0].formatted_address;
+          const location = address.substring(0, address.indexOf(','));
+          console.log(latitude, longitude);
+          console.log(location, address);
 
-  function teste() {
-    console.log('rodou');
-    navigator.geolocation
-      .getCurrentPosition(
-        ({coords: {latitude, longitude}}) => {
           setRegion({
-            region: {
-              latitude,
-              longitude,
-              latitudeDelta: 0.0143,
-              longitudeDelta: 0.0134,
-            },
+            latitude: latitude,
+            longitude: longitude,
+            latitudeDelta: 0.0143,
+            longitudeDelta: 0.0134,
           });
+
+          setUserLocation(location);
+          setUserAddress(address);
+
+          alert(`você está em : ${address}`);
         },
         error => {
-          console.log(error);
+          // See error code charts below.
+          console.log(error.code, error.message);
         },
         {
           timeout: 2000,
           enableHighAccuracy: true,
           maximumAge: 1000,
         },
-      )
-      .then(console.log(region, 'primeiro use effect'));
-  }
-
-  useEffect(() => {
-    console.log(region, 'segundo use effect');
-  }, [region]);
+      );
+    }
+    SetRequestMapCameraChange(false);
+  }, [locationGaranted, requestMapCameraChange]);
 
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
         showsUserLocation={true}
+        region={region}
         onMapReady={async () => {
           await PermissionsAndroid.request(
             PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
           );
-          setIsLoaded(true);
+          setLocationGaranted(true);
+          SetRequestMapCameraChange(true);
         }}
       />
-      <TouchableOpacity
-        style={{alignSelf: 'center', marginTop: 30, elevation: 2}}
-        onPress={() => {
-          setContador(contador + 1);
-        }}>
-        <Text
-          style={{
-            fontSize: 26,
-            fontWeight: 'bold',
-            color: '#ccc',
-          }}>
-          {contador}
-        </Text>
-      </TouchableOpacity>
     </View>
   );
 }
