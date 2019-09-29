2f5e1ccdb80d139f8d52bbdeed0388cb9cc2ce69
yasmmin claudino <ymclaudino@gmail.com>
2019-09-29 14:13:23 -0300

    animal register form

---
+++ src/Pages/AnimalRegister.js
@@ -0,0 +1,228 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  KeyboardAvoidingView,
+  ActivityIndicator,
+  ToastAndroid,
+  Image,
+} from 'react-native';
+
+import * as yup from 'yup';
+import {Formik} from 'formik';
+
+import api from '../services/api.js';
+import { whileStatement } from '@babel/types';
+
+export default function Register({navigation}) {
+  const validationSchema = yup.object().shape({
+    nome: yup
+      .string()
+      .label('Nome')
+      .required('Favor preencher campo nome.'),
+    idade: yup
+      .string()
+      .label('Idade')
+      .required('Favor preencher campo idade'),
+
+    peso: yup
+      .string()
+      .label('Peso')
+      .email('Favor digitar um email válido.')
+      .required('Favor preencher o campo peso.'),
+    raca: yup
+      .string()
+      .label('Raça')
+      .required('Favor preencher o campo raça'),
+  });
+
+
+  /*     
+    ####################################################
+  
+    Verificar se o usuário tá logado
+    recolher id do usuário
+    passar o id na const do animal 
+    cadastrar no banco
+     
+    ####################################################
+
+    
+          
+
+    async function handleRegister(values) {
+    const {nome, peso, idade, raca} = values;
+    try {
+      const usuario = await api.post('/usuario/cadastrarUsuario', {
+        email,
+        senha,
+      });
+      const {id: idusuario} = usuario.data;
+      if (usuario.status === 200) {
+        const pessoa = await api.post('/pessoa/cadastrarPessoa', {
+          nome,
+          cpf,
+          idusuario,
+        });
+        const {id: idpessoa} = pessoa.data;
+        if (pessoa.status === 200) {
+          const cliente = await api.post('/cliente/cadastrarCliente', {
+            idpessoa,
+            idusuario,
+          });
+          if (cliente.status === 200) {
+            navigation.navigate('Login');
+          }
+        }
+      }
+    } catch (error) {
+      return JSON.stringify(error.response.data); //gambiarra pra retornar a message de quando n looga
+    }
+  }
+ */
+  return (
+    <View style={styles.container}>
+      <Formik
+        initialValues={{nome: '', peso: '', idade: '', raca: ''}}
+        onSubmit={async (values, actions) => {
+          const resp = await handleRegister(values); //gambiarra para pegar o valor de quando nao loga
+          if (resp) {
+            ToastAndroid.show(resp, ToastAndroid.SHORT);
+          }
+          setTimeout(() => {
+            actions.setSubmitting(false);
+          }, 1000);
+        }}
+        validationSchema={validationSchema}>
+        {props => (
+          <>
+            <Image
+              source={require('../assets/petspeed-logo-text.png')}
+              resizeMode="center"
+            />
+            <TextInput
+              placeholder="Digite o nome do seu pet"
+              style={styles.input}
+              returnKeyType={'next'}
+              onChangeText={props.handleChange('nome')}
+              blurOnSubmit={false}
+              onSubmitEditing={() => this.cpfRef.focus()} // chama o focus para o proximo
+              onBlur={props.handleBlur('nome')}
+            />
+            <Text style={{color: 'red'}}>
+              {props.touched.nome && props.errors.nome}
+            </Text>
+
+            <TextInput
+              placeholder="Digite o peso do seu pet"
+              style={styles.input}
+              returnKeyType={'next'}
+              onChangeText={props.handleChange('peso')}
+              blurOnSubmit={false}
+              onSubmitEditing={() => this.idadeRef.focus()} // chama o focus para o proximo
+              onBlur={props.handleBlur('peso')}
+              ref={ref => (this.pesoRef = ref)}
+            />
+            <Text style={{color: 'red'}}>
+              {props.touched.peso && props.errors.peso}
+            </Text>
+            <TextInput
+              placeholder="Digite a idade do seu pet"
+              style={styles.input}
+              returnKeyType={'next'}
+              onChangeText={props.handleChange('idade')}
+              blurOnSubmit={false}
+              onSubmitEditing={() => this.racaRef.focus()} // chama o focus para o proximo
+              keyboardType="idade"
+              onBlur={props.handleBlur('idade')}
+              ref={ref => (this.idadeRef = ref)}
+            />
+            <Text style={{color: 'red'}}>
+              {props.touched.idade && props.errors.idade}
+            </Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={props.handleChange('raca')}
+              onBlur={props.handleBlur('raca')}
+              placeholder="Digite a raça do seu pet"
+              secureTextEntry={true}
+              ref={ref => (this.racaRef = ref)} //cria uma referencia desse input
+            />
+            <Text style={{color: 'red'}}>
+              {props.touched.raca && props.errors.raca}
+            </Text>
+            {props.isSubmitting ? (
+              <ActivityIndicator />
+            ) : (
+              <TouchableOpacity
+                style={styles.button}
+                onPress={props.handleSubmit}>
+                <Text style={styles.buttonText}>Cadastrar Pet</Text>
+              </TouchableOpacity>
+            )}
+          </>
+        )}
+      </Formik>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
+    backgroundColor: '#00b894',
+  },
+  input: {
+    alignSelf: 'stretch',
+    borderRadius: 5,
+    fontSize: 16,
+    backgroundColor: '#FAFAF2',
+    marginVertical: 10,
+  },
+  button: {
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    height: 40,
+    marginVertical: 25,
+    marginHorizontal: 20,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#fff',
+  },
+  buttonText: {
+    fontSize: 20,
+    fontFamily: 'Cochin',
+    textAlign: 'center',
+    color: '#FAFAF2',
+    alignContent: 'center',
+    fontWeight: 'bold',
+  },
+  text: {
+    borderStartWidth: 0,
+    marginBottom: -20,
+    fontSize: 12,
+  },
+  textTitle: {
+      fontSize: 35,
+      color: "white",
+      padding: 10,
+      fontFamily: "helvica",
+
+  },
+  passordRecoverContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  alternativeMessage: {
+    marginTop: 40,
+    marginBottom: 20,
+  },
+});
