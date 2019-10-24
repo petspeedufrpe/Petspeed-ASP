fd29fd5698714b5a5fc532430e08f59284325c28
Caio Farias <caiofariaspe@gmail.com>
2019-10-24 20:20:28 -0300

    reactotron host

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -2,7 +2,7 @@ import Reactotron from 'reactotron-react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 
 Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
-  .configure({host: '10.0.0.121'}) // controls connection & communication settings
+  .configure({host: 'localhost'}) // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   .connect();; // let's connect!
 
