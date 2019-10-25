a6d136ad60f0ac830aead6a7817512359ac395d8
YasmminClaudino <yasmmin.claudino@ufrpe.br>
2019-10-25 04:42:30 -0300

    route

---
+++ routes.js
@@ -0,0 +1,52 @@
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import Login from './Pages/Login';
+import Main from './Pages/Main';
+import RegistrationType from './Pages/RegistrationType';
+import AddressForm from './Pages/AddressForm';
+import UserRegister from './Pages/UserRegister';
+import VetRegister from './Pages/VetRegister';
+import UserProfile from './Pages/UserProfile';
+import PetList from './Pages/PetList';
+import VetMain from './Pages/VetMain';
+import AnimalRegister from './Pages/AnimalRegister';
+import AddressPicker from './Pages/AddressPicker';
+import VetOrderCard from './components/VetOrderCard';
+import UserProfileEdit from './Pages/UserProfileEdit'
+import EditPassword from './Pages/EditPassword';
+import AnimalDetails from './Pages/AnimalDetails'
+import AnimalSelect from './Pages/AnimalSelect'
+import OrdemServico from './Pages/OrdemServico'
+import SinptomsList from './Pages/SinptomsList';
+import ListOrdemServicoCliente from './Pages/ListOrdemServicoCliente';
+
+const MainNavigator = createStackNavigator(
+  {
+    Login,
+    VetOrderCard,
+    AddressPicker,
+    VetMain,
+    Main,
+    AnimalRegister,
+    VetRegister,
+    UserRegister,
+    UserProfile,
+    AddressForm,
+    OrdemServico,
+    RegistrationType,
+    PetList,
+    UserProfileEdit,
+    EditPassword,
+    AnimalDetails,
+    AnimalSelect,
+    SinptomsList,
+    ListOrdemServicoCliente,
+  },
+  {
+    headerMode: 'none',
+    navigationOptions: {
+      headerVisible: false,
+    },
+  },
+);
+export default createAppContainer(MainNavigator);
